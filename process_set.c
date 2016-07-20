511600dd287acbaf051b5771c6003240121f01d4
m4n1c22 <sreeram.sadasivam@gmail.com>
2016-07-20 02:09:46 +0100

    process queue added.

--- process_set.c
+++ process_set.c
@@ -11,6 +11,9 @@
 #include <linux/time.h>
 #include <linux/proc_fs.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+
 
 MODULE_AUTHOR("Sreeram Sadasivam");
 MODULE_DESCRIPTION("Process Setting Module");
@@ -19,9 +22,114 @@ MODULE_LICENSE("GPL");
 /** PROC FS RELATED MACROS */
 #define PROC_CONFIG_FILE_NAME	"process_sched_add"
 
+/** STANDARD MACROS */
+#define BASE_10 		10
+
 
 /** Proc FS Dir Object */
 static struct proc_dir_entry *proc_sched_add_file_entry;
+
+/** Structure for a process */
+static struct proc {
+
+	int pid;
+	struct list_head list;
+}top;
+
+/**Function Prototypes for Process Queue Functions*/
+int initialize_process_queue(void);
+int release_process_queue(void);
+int add_process_to_queue(int pid);
+int remove_process_from_queue(int pid);
+int print_process_queue(void);
+
+
+/** Process Queue Functions */
+
+/**
+	Function Name : initialize_process_queue
+	Function Type : Queue Function
+	Description	  :	Method is invoked for initializing a process queue.
+*/
+int initialize_process_queue(void) {
+
+	printk(KERN_INFO "Initializing the Process Queue...\n");
+	INIT_LIST_HEAD(&top.list);
+	return 0;
+}
+
+/**
+	Function Name : release_process_queue
+	Function Type : Queue Function
+	Description	  :	Method is invoked for releasing a process queue.
+*/
+int release_process_queue(void) {
+		 	
+	struct proc *tmp, *node;
+	printk(KERN_INFO "Releasing Process Queue...\n");
+	list_for_each_entry_safe(node, tmp, &(top.list), list) {
+	
+		list_del(&node->list);
+		kfree(node);
+	}
+	return 0;
+}
+
+/**
+	Function Name : add_process_to_queue
+	Function Type : Queue Function
+	Description	  :	Method is invoked for adding a process into a queue.
+*/
+int add_process_to_queue(int pid) {
+			
+	struct proc *new_process = kmalloc(sizeof(struct proc), GFP_KERNEL);
+	
+	new_process->pid = pid;
+	
+	INIT_LIST_HEAD(&new_process->list);
+	
+	list_add_tail(&(new_process->list), &(top.list));
+	
+	return 0;
+}
+
+/**
+	Function Name : remove_process_from_queue
+	Function Type : Queue Function
+	Description	  :	Method is invoked for removing a given process 
+					from the queue.
+*/
+int remove_process_from_queue(int pid) {
+		 	
+	struct proc *tmp, *node;
+	
+	list_for_each_entry_safe(node, tmp, &(top.list), list) {
+	
+		if(node->pid == pid) {
+			printk(KERN_INFO "Removing the given Process %d from the  Process Queue...\n", pid);
+			list_del(&node->list);
+			kfree(node);
+		}
+	}
+	return 0;
+}
+
+
+/**
+	Function Name : print_process_queue
+	Function Type : Queue Function
+	Description	  :	Method is invoked for adding a process into a queue.
+*/
+int print_process_queue(void) {
+			
+	struct proc *tmp;
+	printk(KERN_INFO "Process Queue: \n");
+	list_for_each_entry(tmp, &(top.list), list) {
+	
+		printk(KERN_INFO "Process ID: %d\n", tmp->pid);
+	}
+	return 0;
+}
 
 
 /**
@@ -39,7 +147,7 @@ static ssize_t process_sched_add_module_read(struct file *file, char *buf, size_
 {
 	
 	printk(KERN_INFO "Process Scheduler Add Module read.\n");
-	
+	print_process_queue();
 	/** Successful execution of read call back. EOF reached.*/
 	return 0;
 }
@@ -57,8 +165,20 @@ static ssize_t process_sched_add_module_read(struct file *file, char *buf, size_
 */
 static ssize_t process_sched_add_module_write(struct file *file, const char *buf, size_t count, loff_t *ppos)
 {
+	int ret;
+	long int new_proc_id;	
+	
 	printk(KERN_INFO "Process Scheduler Add Module write.\n");
 	printk(KERN_INFO "Registered Process ID: %s\n", buf);
+	
+	ret = kstrtol(buf,BASE_10,&new_proc_id);
+	if(ret < 0) {
+		/** Invalid argument in conversion error.*/
+		return -EINVAL;
+	}
+	
+	add_process_to_queue(new_proc_id);
+	
 	/** Successful execution of write call back.*/
 	return count;
 }
@@ -108,13 +228,13 @@ static struct file_operations process_sched_add_module_fops = {
 };
 
 /**
-	Function Name : gen_module_init
+	Function Name : process_scheduler_module_init
 	Function Type : Module INIT
 	Description   : Initialization method of the Kernel module. The
 			method gets invoked when the kernel module is being
 			inserted using the command insmod.
 */
-static int __init gen_module_init(void)
+static int __init process_scheduler_module_init(void)
 {
 	printk(KERN_INFO "Process Scheduler module is being loaded.\n");
 	
@@ -126,24 +246,30 @@ static int __init gen_module_init(void)
 		/** File Creation problem.*/
 		return -ENOMEM;
 	}
+	
+	/**Initializing the process queue*/
+	initialize_process_queue();
+	
 	/** Successful execution of initialization method. */
 	return 0;
 }
 
 /**
-	Function Name : gen_module_cleanup
+	Function Name : process_scheduler_module_cleanup
 	Function Type : Module EXIT
 	Description   : Cleanup method of the Kernel module. The
                 	method gets invoked when the kernel module is being
                  	removed using the command rmmod.
 */
-static void __exit gen_module_cleanup(void)
+static void __exit process_scheduler_module_cleanup(void)
 {
+	
+	release_process_queue();
 	printk(KERN_INFO "Process Scheduler module is being unloaded.\n");
 	/** Proc FS object removed.*/
 	proc_remove(proc_sched_add_file_entry);
 }
 /** Initializing the kernel module init with custom init method */
-module_init(gen_module_init);
+module_init(process_scheduler_module_init);
 /** Initializing the kernel module exit with custom cleanup method */
-module_exit(gen_module_cleanup);
+module_exit(process_scheduler_module_cleanup);
